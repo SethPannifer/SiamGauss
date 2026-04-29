590c503b36a85c391bfe39e2a020202c2ab007a7
SethPannifer <149721599+SethPannifer@users.noreply.github.com>
2026-04-29 13:34:07 +0100

    Delete data_loader.py

--- SiamGauss/Benchmarks/mpbench/core/data_loader.py
+++
@@ -1,8 +0,0 @@
-import os
-from scipy.io import loadmat
-
-
-def load_mat_file(filename):
-    base_path = os.path.dirname(os.path.dirname(__file__))
-    file_path = os.path.join(base_path, filename)
-    return loadmat(file_path)
